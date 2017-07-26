5a76f38659fbf67baab247d893462fb962afdf3b
Jonathan Lintott <jonathan@suade.org>
2017-07-26 17:55:46 +0100

    Part one docker config

--- project/__init__.py
+++ project/__init__.py
@@ -1,5 +1,6 @@
 # project/__init__.py
 
+import os
 from flask import Flask, jsonify
 
 
@@ -7,6 +8,7 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 # Set config
+app_settings = os.getenv('APP_SETTINGS')
 app.config.from_object('project.config.DevelopmentConfig')
 
 
